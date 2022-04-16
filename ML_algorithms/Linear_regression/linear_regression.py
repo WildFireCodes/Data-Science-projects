0679fcdfda7bc2790e90208f3441052db4652b35
WildFireCodes <fifada13@gmail.com>
2022-04-16 18:37:49 +0200

    RMSE fix

--- ML_algorithms/Linear_regression/linear_regression.py
+++ ML_algorithms/Linear_regression/linear_regression.py
@@ -12,8 +12,8 @@ class LinearRegression:
         self.slope = np.zeros(n)
 
         for _ in range(self.n_iterations):
-            self.slope -= self.n_learning_rate * (-2/n) * self.calc_slope(X_train.T, y_train)
-            self.bias -= self.n_learning_rate * (-2/n) * self.calc_bias(X_train.T, y_train)
+            self.slope -= self.n_learning_rate * (-2/n) * self.calc_slope(X_train, y_train)
+            self.bias -= self.n_learning_rate * (-2/n) * self.calc_bias(X_train, y_train)
 
     def predict(self, X):
         return self.regression(X)
@@ -22,12 +22,10 @@ class LinearRegression:
         return np.sum(y - self.regression(X))
 
     def calc_slope(self, X, y):
-        # return np.sum(X * (y - self.regression(X)))
-        return np.dot(X, (y - self.regression(X)))
+        return np.dot(X.T, (y - self.regression(X)))
 
     def regression(self, X):
-        # return self.slope * X + self.bias
-        return np.dot(self.slope, (X + self.bias))
+        return np.dot(X, self.slope) + self.bias
 
 
     
